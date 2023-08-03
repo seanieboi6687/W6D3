4b010c0f829ff9fa60ff26d9945be15307e3d4cd
Sean <pca.sean.jeun@gmail.com>
2023-08-03 11:33:19 -0400

    artworks_controller created

---
+++ practice-for-ch-rails-first-routes-long-practice-main/First_Route_And_Controllers/app/controllers/artworks_controller.rb
@@ -0,0 +1,39 @@
+class ArtworksController < ApplicationController
+    def index
+        @artworks = Artwork.all
+        render json: @artworks
+    end
+
+    def create
+        artwork = Artwork.new(self.artwork_params)
+        if artwork.save
+            render json: artwork
+        else
+            render json: artwork.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        artwork = Artwork.find(params[:id])
+        render json: artwork
+    end
+
+    def update
+        artwork = Artwork.find(params[:id])
+        artwork.update(self.artwork_params)
+        render json: artwork
+    end
+
+    def destroy
+        artwork = Artwork.find(params[:id])
+        artwork.destroy
+        render json: "Delete Succesful"
+    end
+
+    private
+
+    def artwork_params
+        params.require(:artwork).permit(:title)
+    end
+
+end
