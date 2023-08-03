bedc3629be30cefba568f0540074a2be1c84c039
Sean <pca.sean.jeun@gmail.com>
2023-08-03 11:26:58 -0400

    routes.rb optimized

--- practice-for-ch-rails-first-routes-long-practice-main/First_Route_And_Controllers/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/First_Route_And_Controllers/config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # resources :users
 
-  get 'users', to: 'users#index'
-  post 'users', to: 'users#create'
-  get 'users/new', to: 'users#new'
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  patch 'users/:id', to: 'users#update'
-  put 'users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
+  resources :users, only: [:create, :destroy, :index, :show, :update]
+
+  # get 'users', to: 'users#index'
+  # post 'users', to: 'users#create'
+  # get 'users/new', to: 'users#new'
+  # get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get 'users/:id', to: 'users#show', as: 'user'
+  # patch 'users/:id', to: 'users#update'
+  # put 'users/:id', to: 'users#update'
+  # delete 'users/:id', to: 'users#destroy'
 end
